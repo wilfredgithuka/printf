5b77ae7e2526684b83a85229424e857e6c9a1e58
wilfredgithuka <wilfred.gachugi@gmail.com>
2023-06-27 13:19:16 +0300

    Undefined Ref to Printf

--- main.h
+++ main.h
@@ -1,97 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
-#define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+#define UNUSED(x) (void)(x)
 
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
+/* Some bit of Flags */
+
 #define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
+#define F_MINUS 1
 
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
-
-/**
- * struct fmt - Struct op
- *
- * @fmt: The format.
- * @fn: The function associated.
- */
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-
-typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-
-va_list list, char buffer[], int flags, int width, int precision, int size);
-
-int printf_char(va_list val);
-
 
 /* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-int print_pc(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+int print_char(va_list x, char buffer[], int flags, int width);
+int print_string(va_list y, char buffer[]);
+int print_pc(va_list z, char buffer[]);
 
-/* Functions to print numbers */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-
+/*Print handlers*/
 /* width handler */
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
+int handle_print_char(char c, char buffer[], int flags, int width);
 
-int write_unsgnd(int is_negative, int ind,
-char buffer[],
-	int flags, int width, int precision, int size);
-
-
-
-long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num, int size);
-
-#endif
--- print_whenever.c
+++ print_whenever.c
@@ -3,26 +3,34 @@
 /**
  * print_char - Function to prints a char[c]
  * @x: List a of args given
+ * @buffer: This is an array to handle the print characters
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
  * Return: Count of no of chars printed
  */
 
-int print_char(va_list x, char_buffer[])
+int print_char(va_list x, char buffer[], int flags, int width)
 {
 	char c = va_arg(x, int);
 
-	return (handle_char(c));
+	return (handle_print_char(c, buffer, flags, width));
 }
 
 /**
  * print_string - Function to print a string[s]
  * @y: List of args given
+ * @buffer: This is an array to handle the print characters.
  * Return: Count of no of strings printed
  */
 
-int print_string(va_list y, char_buffer[])
+int print_string(va_list y, char buffer[])
 {
-	int str_length = 0, i;
-	char *str = va_arg(types, char *);
+	int str_length = 0;
+	char *str = va_arg(y, char *);
+
+	UNUSED(buffer);
 
 	while (str[str_length] != '\0')
 		str_length++;
@@ -39,6 +47,8 @@ int print_string(va_list y, char_buffer[])
 
 int print_pc(va_list z, char buffer[])
 {
+	UNUSED(z);
+	UNUSED(buffer);
 	return (write(1, "%%", 1));
 }
 
